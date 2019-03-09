a7d08a78a0794da812f64f7e84cc62930c69f48f
unknown <chugm1989@gmail.com>
2019-03-09 09:06:19 -0500

    assignment_submission

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,13 @@
 // GLOBAL VARIABLES
-//document.ready(function() {
-
-
 var amethystCounter = 0;
 var citrineCounter = 0;
 var moonstoneCounter = 0;
 var sapphireCounter = 0;
-
 var userTotal = 0;
+var winsCounter = 0;
+var lossesCounter = 0;
+const winCheck = [];
+
 
 
 // CODE FOR GEM VALUE GENERATOR 
@@ -24,83 +24,67 @@ var sapphireNum = Math.floor(Math.random() * 12) + 1;
 console.log(sapphireNum);
 
 // var compNum = Math.floor(Math.random() * 120) + 19;
- console.log(compNum);
-function compNum(max) {
+/* console.log(compNum);
+ function compNum(max) {
   var userWin = Math.floor(Math.random() * Math.floor(121));
   return Math.floor(Math.random() * Math.floor(121));
-};
-$("#winScore").html(compNum(" '<p> Winning Score </p>' " + 121));
-// WIN CONDITION SCORE
-$("#winScore").html(compNum);
+}; */
 
+var userWin = {
+    winNum: Math.floor(Math.random() * Math.floor(121)),
+};
+console.log(userWin.winNum);
+var userWinCondition = userWin.winNum;
+console.log(userWinCondition);
+
+
+
+// STARTS ALL OTHER EVENTS AND FUNCTIONS
+$(document).ready(function() {
+
+// $("#winScore").html(compNum(" '<p> Winning Score </p>' " + 121));
+$("#winScore").html(userWin.winNum);
 // BUTTONS
 // #id and button click works
 $("#amethystButton").click(function() {
-    userTotal += amethystNum;
+    var winCondition = userTotal += amethystNum;
     $("#userTally").html(userTotal);
-    
+    winCheck.push(winCondition);
+    const forTheWin = (accumulator, currentValue) => accumulator + currentValue;
+    console.log(winCheck.reduce(forTheWin));
+    var doYouWin = winCheck.reduce(forTheWin);
 //    wincheck();
 });
 
 $("#citrineButton").click(function() {
     userTotal += citrineNum;
     $("#userTally").html(userTotal);
- //   alert(citrineTotal);
+    winCheck.push(winCondition);
+    const forTheWin = (accumulator, currentValue) => accumulator + currentValue;
+    console.log(winCheck.reduce(forTheWin));
+    var doYouWin = winCheck.reduce(forTheWin);
 });
 
 $("#moonstoneButton").click(function() {
     userTotal += moonstoneNum;
     $("#userTally").html(userTotal);
- //   alert(moonstoneTotal);
+    winCheck.push(winCondition);
+    const forTheWin = (accumulator, currentValue) => accumulator + currentValue;
+    console.log(winCheck.reduce(forTheWin));
+    var doYouWin = winCheck.reduce(forTheWin);
 });
 
 $("#sapphireButton").click(function() {
     userTotal += sapphireNum;
     $("#userTally").html(userTotal);
-  //  alert(sapphireTotal);
+    winCheck.push(winCondition);
+    const forTheWin = (accumulator, currentValue) => accumulator + currentValue;
+    var doYouWin = winCheck.reduce(forTheWin);
 });
 
-// NOT WORKING
-/*function amethystInsert(amethystClick) {
-    var amethystClick = document.getElementById('amethystButton');
-    return amethystNum;
-};
-console.log(amethystClick); */
-
-// FUNCTION TO CREATE GAME WIN NUMBER
-/* THIS DOESN'T WORK
-function compNum2(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max-min) + min);
-};
-console.log(compNum2);
-*/
-
-/* var compNum = Math.floor(Math.random()*101) + 19         THIS WORKS
-console.log(compNum); */
-
-// CONSOLE LOGS BOOLEAN
-// var compNumLim = (compNum <= 120);
-// console.log(compNumLim);
-
-var winsCounter = 0;
-var lossesCounter = 0;
-// FUNCTION TO ADD TO WINS/LOSSES
-// $(".class").on("click", function() {
-
-// LOGIC
-
-// On "click" <button> event, insert random number and add random number to current user score
-//window.onclick = function(gem1, gem2, gem3, gem4) {
-// PUT THIS IN THE DIV CONTAINER, USE #iD SELECTOR
- //    var clickAmethyst = amethyst.click;
-   //  console.log(clickAmethyst);
- // DON'T NEED, IT'S THE GEM BUTTONS THAT NEED TO CLICK TO GENERATE NUMBERS    
- /*    document.onclick = function(event) {
-    var eventClick = event.click;
-    console.log(eventClick); */
- 
-
-// MAIN PROCESS
+//  **COULD NOT COMPLETE GAME WIN CONDITION LOGIC**
+    if (doYouWin === userWinCondition) {
+        alert("You suck!")
+    }
+});
 
